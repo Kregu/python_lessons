b7675c26d4f39915296586b441ce39daf6f1579b
Sergey Gorokhov <sergey.login@gmail.com>
2020-02-24 20:29:47 +0300

    Object refactoring

--- ball/ball.py
+++ ball/ball.py
@@ -5,39 +5,50 @@ WIDTH = 640
 HEIGTH = 480
 
 
+class Ball:
+    def __init__(self):
+        self.dx = self.dy = 1
+        self.R = randint(20, 50)
+        self.x = randint(self.R, WIDTH - self.R)
+        self.y = randint(self.R, HEIGTH - self.R)
+        self.ball_id = canvas.create_oval(self.x - self.R, self.y - self.R,
+                                          self.x + self.R, self.y + self.R,
+                                          fill="green")
+
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+
+        if self.x + self.R > WIDTH or self.x - self.R < 0:
+            self.dx = - self.dx
+        if self.y + self.R > HEIGTH or self.y - self.R < 0:
+            self.dy = - self.dy
+
+    def show(self):
+        canvas.move(self.ball_id, self.dx, self.dy)
+
+
+
 def canvas_click_handler(event):
     print('Hello! x=', event.x, 'y=', event.y)
 
 
 def tick():
-    global x, y, dx, dy
-    x += dx
-    y += dy
-
-    if x + R > WIDTH or x - R < 0:
-        dx = - dx
-    if y + R > HEIGTH or y - R < 0:
-        dy = - dy
-
-    canvas.move(ball_id, dx, dy)
+    ball.move()
+    ball.show()
     root.after(10, tick)
 
 
 def main():
-    global root, canvas
-    global ball_id, x, y, dx, dy, R  # TODO
+    global root, canvas, ball
 
-    dx = dy = 1
     root = tk.Tk()
     root.geometry(str(WIDTH) + "x" + str(HEIGTH))
     canvas = tk.Canvas(root, width=WIDTH, height=HEIGTH, background="white")
     canvas.pack()
     canvas.bind('<Button-1>', canvas_click_handler)
 
-    R = randint(20, 50)
-    x = randint(R, WIDTH - R)
-    y = randint(R, HEIGTH - R)
-    ball_id = canvas.create_oval(x - R, y - R, x + R, y + R, fill="green")
+    ball = Ball()
 
     tick()
     root.mainloop()
